88cbd404fd11f02a9f5164849877270c83daf11e
maevadesfours <maeva.desfours@orange.fr>
2024-10-14 12:28:37 +0200

    first commit

--- pages/xstatepolyline/xStatePolyline.js
+++ pages/xstatepolyline/xStatePolyline.js
@@ -2,25 +2,64 @@ import Konva from "konva";
 import { createMachine, interpret } from "xstate";
 
 
+
+
 const stage = new Konva.Stage({
     container: "container",
     width: 400,
     height: 400,
 });
 
+
 const layer = new Konva.Layer();
 stage.add(layer);
+
 
 const MAX_POINTS = 10;
 let polyline // La polyline en cours de construction;
 
+
 const polylineMachine = createMachine(
     {
-        /** @xstate-layout N4IgpgJg5mDOIC5gF8A0IB2B7CdGgAcsAbATwBkBLDMfEI2SgF0qwzoA9EBaANnVI9eAOgAM4iZMkB2ZGnokK1MMMoRitJAsYs2nRABYATAMQAOAIzCD0gJwXetgwGZezgw9ty5QA */
+        /** @xstate-layout N4IgpgJg5mDOIC5QAcD2AbAngGQJYDswA6XCdMAYgFkB5AVQGUBRAYWwEkWBpAbQAYAuohSpYuAC65U+YSAAeiAIwAmAKxEALAE4dW1QGZFAdgAci-Ro0AaEJiWrlRZcoBsqg6qPL9RvhpMAvgE2aFh4hETSYAAKqATi1PTMtABqTPxCSCBoYpLSsgoIyib6RG4uytraqsZG1raIZpq6JtUuWm4GQSEYOATEUbHxiYysHNwZsjkSUjJZhZaKRBY+-qZ1vqo2dgiKqlqaRm76Jvtarnvd2b3hxOj9APoAtgCG+JhD+OKwFABCAILcBjRQHpQRTUQzfLzRAuPyHVwVVatLTbRomIh1HQmNSKRR+dwuILBED4VAQOAQsL9CG5WYFRAAWhcaIQzKIfE5XO53KMV1CfQipHItKhc1AC2UrOM6l8hmUihMWj4ipc-n5N36kUIn3Eory4vkiH0fFKCpMRhqLiMNrUJmlOKIekMfHaxj4NtUGupEXuhGebw+cS+8Cy0wNDIQcI0RFU3OUHX8Fi2DV2yqcpvxliMeM8Gj5xKAA */
         id: "polyLine",
         initial: "idle",
         states : {
             idle: {
+                on: {
+                    MOUSECLICK: {
+                        target: "onePoint",
+                        actions: "createLine"
+                    }
+                }
+            },
+
+
+            onePoint: {
+                on: {
+                    MOUSEMOVE: {
+                        target: "onePoint",
+                        internal: true,
+                        actions: "setLastPoint"
+                    },
+
+
+                    MOUSECLICK: {
+                        target: "line_manyPoints",
+                        actions: "addPoint"
+                    }
+                }
+            },
+
+
+            line_manyPoints: {
+                on: {
+                    BACKSPACE: {
+                        target: "line_manyPoints",
+                        internal: true,
+                        actions: "removeLastPoint"
+                    }
+                }
             }
         }
     },
@@ -42,6 +81,7 @@ const polylineMachine = createMachine(
                 const pos = stage.getPointerPosition();
                 const currentPoints = polyline.points(); // Get the current points of the line
                 const size = currentPoints.length;
+
 
                 const newPoints = currentPoints.slice(0, size - 2); // Remove the last point
                 polyline.points(newPoints.concat([pos.x, pos.y]));
@@ -67,7 +107,7 @@ const polylineMachine = createMachine(
             // Abandonner le tracé de la polyline
             abandon: (context, event) => {
                 // Supprimer la variable polyline :
-                
+               
             },
             // Supprimer le dernier point de la polyline
             removeLastPoint: (context, event) => {
@@ -84,7 +124,7 @@ const polylineMachine = createMachine(
             pasPlein: (context, event) => {
                 // Retourner vrai si la polyline a moins de 10 points
                 // attention : dans le tableau de points, chaque point est représenté par 2 valeurs (coordonnées x et y)
-                
+               
             },
             // On peut enlever un point
             plusDeDeuxPoints: (context, event) => {
@@ -94,6 +134,7 @@ const polylineMachine = createMachine(
         },
     }
 );
+
 
 // On démarre la machine
 const polylineService = interpret(polylineMachine)
@@ -106,9 +147,11 @@ stage.on("click", () => {
     polylineService.send("MOUSECLICK");
 });
 
+
 stage.on("mousemove", () => {
     polylineService.send("MOUSEMOVE");
 });
+
 
 // Envoi des touches clavier à la machine
 window.addEventListener("keydown", (event) => {
@@ -116,3 +159,8 @@ window.addEventListener("keydown", (event) => {
     // Enverra "a", "b", "c", "Escape", "Backspace", "Enter"... à la machine
     polylineService.send(event.key);
 });
+
+
+
+
+
